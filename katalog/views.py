9eb574bbdafe6edbfdf886cc99a004ef0731c918
nicolepanggabeann <nicole.panggabean@gmail.com>
2022-09-14 23:57:34 +0700

    Completed routing and data mapping

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+from gettext import Catalog
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_catalog_item,
+        'name': 'Theirry Nicole Panggabean',
+        'id': '2106721004',
+    }
+    return render(request, "katalog.html", context)
