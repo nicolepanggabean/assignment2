3bc3e0e9e8dbcaf06f2892b970f150071d2894fe
nicolepanggabeann <theirry.nicole@ui.ac.id>
2022-09-19 22:18:40 +0700

    Revert "Finished editing mywatchlist files"
    This reverts commit 8429db7445af73d78f427bb9a2a0f18a0f28ab18.

--- mywatchlist/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class MyWatchList(models.Model):
-    watched = models.BooleanField()
-    title = models.CharField(max_length=50)
-    rating = models.IntegerField()
-    release_date = models.IntegerField()
-    review = models.TextField()
