a1b25a0e3ca0f76e47f7bb96d270ffb3efe6793c
nicolepanggabeann <theirry.nicole@ui.ac.id>
2022-09-21 21:01:28 +0700

    restarting

--- mywatchlist/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class MyWatchList(models.Model):
-    watched = models.BooleanField()
-    title = models.CharField(max_length=50)
-    rating = models.FloatField()
-    review = models.TextField()
--- project_django/settings.py
+++ project_django/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'example_app',
     'katalog',
-    'mywatchlist',
 ]
 
 MIDDLEWARE = [
