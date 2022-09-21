66ef4ce5a236fdc6ed30b20a771882f0cd139488
nicolepanggabeann <theirry.nicole@ui.ac.id>
2022-09-21 19:33:15 +0700

    Changed rating and release_date field type

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class MyWatchList(models.Model):
     watched = models.BooleanField()
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
-    release_date = models.DateField()
+    rating = models.FloatField()
+    release_date = models.TextField()
     review = models.TextField()
